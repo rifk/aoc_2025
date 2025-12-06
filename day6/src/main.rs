e547f7cde207415d737a63b358c6356b5e75ddb5
rifk <rifuku.kobayashi@gmail.com>
2025-12-06 10:15:52 +0000

    add day6

---
+++ day6/src/main.rs
@@ -0,0 +1,11 @@
+use utils::derive::aoc;
+
+#[aoc(day6, part1)]
+fn solve_one(input: &str) -> anyhow::Result<String> {
+    day6::solve_one(input)
+}
+
+#[aoc(day6, part2)]
+fn solve_two(input: &str) -> anyhow::Result<String> {
+    day6::solve_two(input)
+}
